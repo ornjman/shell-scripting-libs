f4d042be5082cdbf473de0b0f0b8f589fc37bb69
ornjman <ornjman@gmail.com>
2017-02-15 23:20:08 -0500

    providing arguments library for deal with arguments in shell scripts

--- arguments.sh
+++ arguments.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# function to require at least one argument passed to a function
 function require_arguments(){
+
+  # check if the number of arguments is lt 1
   if [ "$#" -lt 1 ]; then
+    
+    # output errors on stderr
     echo "No Argument(s) Provided." >&2
+
+    # exit the script
     exit 1
   fi
 }
